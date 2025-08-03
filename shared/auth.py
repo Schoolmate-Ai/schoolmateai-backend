1a8d2bc6b28b2cbae541ac390cc4eda6d93cabeb
SKKEVWD <amit.kumar2@embitel.com>
2025-08-03 17:14:47 +0530

    increased time in token exp

--- shared/auth.py
+++ shared/auth.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",2880)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
